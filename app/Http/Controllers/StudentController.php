eff8a196b63c8b778ec69abb928de2a3f3bc20c6
SalindaGIT <salindakas@gmail.com>
2023-12-03 16:07:26 +0530

    StudentController data saving

---
+++ app/Http/Controllers/StudentController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Student;
+use Illuminate\Http\Request;
+
+class StudentController extends Controller
+{
+    public function store(Request $request){
+        $file = $request->file('avatar');
+        $fileName = time() . '.' . $file->getClientOriginalExtension();
+        $file->storeAs('public/images',$fileName);
+
+        $student = Student::create([
+            'avatar'=>$fileName,
+            'name'=>$request->name,
+            'email'=>$request->email,
+        ]);
+
+        return response()->json([
+            'status' => 200,
+        ]);
+
+
+
+    }
+}
